aa0d06e7e3ff3baa505ce8bb0fe27e9d267e3157
kareem22T <m.othman@clicksegypt.net>
2024-11-04 19:40:09 +0200

    edits

--- app/Filament/Resources/ApplicationResource/Pages/ListApplications.php
+++ app/Filament/Resources/ApplicationResource/Pages/ListApplications.php
@@ -13,7 +13,6 @@ class ListApplications extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
         ];
     }
 }
