ddef991958dba33c3315d965ebf55e0126dcd011
kareem22T <m.othman@clicksegypt.net>
2024-11-05 21:52:49 +0200

    edtis

--- resources/views/filament/resources/application-resource/widgets/image-widget.blade.php
+++ resources/views/filament/resources/application-resource/widgets/image-widget.blade.php
@@ -1,5 +1,5 @@
 <x-filament-widgets::widget>
         <div class="flex justify-center flex-direction-column mb-3" style="flex-direction: column;gap: 1rem;width: max-content;">
-            <img src="{{ $record->user->photo ? asset('storage/' . $record->user->photo) : asset('/$record->user->photo.jpg') }}" alt="Profile Image" class="w-32 h-32 rounded-full d-block" />
+            <img src="{{ $record->user->photo ? asset('storage/' . $record->user->photo) : asset('/default-avatar-icon-of-social-media-user-vector.jpg') }}" alt="Profile Image" class="w-32 h-32 rounded-full d-block" />
         </div>
 </x-filament-widgets::widget>
